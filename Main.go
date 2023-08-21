ca9a651eb3c1ad65678849a7129f5f91c3cfa614
felpsbc <fbcarvalho2004@gmail.com>
2023-08-21 19:14:49 -0300

    Ex01 golang

---
+++ Main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var numero1, numero2, numero3 int
+
+	fmt.Println("Escreva o primeiro numero: ")
+	fmt.Scan(&numero1)
+	fmt.Println("Escreva o segundo numero: ")
+	fmt.Scan(&numero2)
+	fmt.Println("Escreva o terceiro numero: ")
+	fmt.Scan(&numero3)
+
+	soma := numero1 + numero2 + numero3
+
+	fmt.Println("A soma dos números é ? ", soma)
+
+}
