da0b20f11c3001e2e98c9b7c2c64fab21bbb2e8e
felpsbc <fbcarvalho2004@gmail.com>
2023-08-28 15:53:38 -0300

    Exercícios

--- Repetição/Ex06_Repetição.go
+++ Repetição/Ex06_Repetição.go
@@ -1,5 +1,14 @@
 package main
 
+import "fmt"
+
 func main() {
 
+	var num int
+	fmt.Println("Escreva o numero que deseja mulitplicar :")
+	fmt.Scan(&num)
+
+	for mult := 1; mult <= 10; mult++ {
+		fmt.Println("O resultado da sua tabuada e", num*mult)
+	}
 }
