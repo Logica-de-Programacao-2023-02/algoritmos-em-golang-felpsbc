16d85d519850fc5530fd14f2d4e3f7fdd1dfdc49
felpsbc <fbcarvalho2004@gmail.com>
2023-08-29 15:37:34 -0300

    Exercícios

--- Repetição/Ex08_Repetição.go
+++ Repetição/Ex08_Repetição.go
@@ -1,5 +1,17 @@
 package main
 
+import "fmt"
+
 func main() {
+
+	var num int
+	fmt.Println("Escreva um número: ")
+	fmt.Scan(&num)
+
+	for i := 1; i <= num; i++ {
+		if num%i == 0 {
+			fmt.Println("Os divisores de", num, "são", i)
+		}
+	}
 
 }
--- Repetição/Ex09_Repetição.go
+++ Repetição/Ex09_Repetição.go
@@ -1,5 +1,18 @@
 package main
 
+import "fmt"
+
 func main() {
 
+	var soma, div, i float64
+
+	for {
+		fmt.Println("Escreva um número: ")
+		fmt.Scan(&i)
+	}
+	if i == 0 {
+		break
+	}
+	soma += i;
+	div++
 }
